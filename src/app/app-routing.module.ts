37079dae4844a2ef0fe09d88624fdafdab0a91ef
djordialba321 <djordialba321@gmail.com>
2023-11-13 19:45:49 +0700

    tugas6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+{
+path: 'home',
+loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+},
+{
+path: '',
+redirectTo: 'login',
+pathMatch: 'full'
+},
+{
+path: 'login',
+loadChildren: () => import('./login/login.module').then( m =>
+m.LoginPageModule),
+canLoad: [AutoLoginGuard]
+},
+{
+path: 'dashboard',
+loadChildren: () => import('./dashboard/dashboard.module').then( m =>
+m.DashboardPageModule),
+canLoad: [AuthGuard] // Secure all child pages
+},
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+imports: [
+RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+],
+exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
